a1f529b6ef9e0edba384aef77a896d8e057624d8
PrajwalSinghBhadouria <151111886+PrajwalSinghBhadouria@users.noreply.github.com>
2025-01-15 19:01:22 +0530

    Add files via upload

---
+++ Square_root.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// Function to find square root
+// x: element to find square root
+class Solution {
+  public:
+    int floorSqrt(int n) {
+        // Your code goes here
+        int ans;
+        for(int i=1; i<=(n/2)+1; i++){
+            if(i*i==n){
+                ans = i;
+                break;
+            }
+            if(i*i>n){
+                ans = i-1;
+                break;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution obj;
+        cout << obj.floorSqrt(n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
